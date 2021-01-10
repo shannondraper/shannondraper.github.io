40b27412ecd8d87f51c81982e51a59cb8889455a
ShannonDraper <shan.draper@gmail.com>
2021-01-10 16:48:49 -0500

    removed subdirectories

--- app.js
+++ app.js
@@ -10,26 +10,10 @@ app.use('/scripts', express.static(__dirname + 'public/scripts'));
 app.use('/assets', express.static(__dirname + 'public/assets'));
 app.use('/fonts', express.static(__dirname + 'public/fonts'));
 
-// for mojo subdirectory
-// app.use(express.static('views/mojo'));
-// app.use('/css', express.static(__dirname + 'views/mojo/css'));
-// app.use('/assets', express.static(__dirname + 'views/mojo/assets'));
-// app.use('/icofont', express.static(__dirname + 'views/mojo/icofont'));
-
-// app.use(express.static('views/totalEclipseOfTheHeart'));
-// app.use('/style', express.static(__dirname + 'views/totalEclipseOfTheHeart/style'));
-// app.use('/assets', express.static(__dirname + 'views/totalEclipseOfTheHeart/assets'));
-
 // Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// app.get('/mojo', (req, res) => {
-//     res.render('mojo/index');
-// });
-// app.get('/totaleclipseoftheheart', (req, res) => {
-//     res.render('totalEclipseOfTheHeart/index');
-// });
 app.get('', (req, res) => {
     res.render('index');
 });
